140920f894f4b5fa7ccf8c624b2b136596fa7209
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-07-23 09:49:20 +0300

    Add files via upload

---
+++ potoki_na_klassah_3.py
@@ -0,0 +1,16 @@
+
+class Knight:
+    INVADERS = 100
+    day = 0
+    skills = 20
+
+    def run(self):
+        while self.INVADERS > 0:
+            self.day = self.day + 1
+            self.INVADERS = self.INVADERS - self.skills
+            print(f"сражается {self.day} дней, осталось {self.INVADERS} воинов")
+
+
+my_obj = Knight()
+my_obj.run()
+print(f"Рыцарь одержал победу спустя {my_obj.day} дней")
